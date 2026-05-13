268ad22d32fd92068995456ced59d0401d284172
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-05-13 02:18:44 +0200

    Add config.py file with paths to resources

---
+++ config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+PROJ_ROOT = Path(__file__).resolve().parents[0]
+DATA_DIR = PROJ_ROOT / "data"
+
+RAW_DATA_DIR = DATA_DIR / "raw"
+
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+FINAL_DATASET = PROCESSED_DATA_DIR / "final_dataset_with_all_features_v3.1.csv"
